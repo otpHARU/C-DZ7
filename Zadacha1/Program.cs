65665c2abe18c6c5ba8ede7f25367b2160998bd4
Haru <harumolodec@mail.ru>
2022-08-16 01:22:29 +0700

    Zadacha1 END

---
+++ Zadacha1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+double[,] matrix = new double[rows, columns];
+
+Random random = new Random();
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = random.NextDouble() * 10;
+        Console.Write("{0,6:F2}", matrix[i, j]);
+    }
+    Console.WriteLine();
+}
