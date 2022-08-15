76d59bf5b8df3d491e5837f4fdcec4a16ff19d3a
Haru <harumolodec@mail.ru>
2022-08-16 03:10:25 +0700

    Zadacha3 END

---
+++ Zadacha3/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+int[,] array = new int[rows, columns];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(0, 100);
+        Console.Write(array[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    int srAr = 0;
+
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        srAr += array[j, i];
+    }
+    Console.WriteLine($"Cреднее арифметическое элементов столбца {i + 1} = {(float)srAr / array.GetLength(0)}");
+}
+
+
+
+
+
+
+
+
+
+
+
+
